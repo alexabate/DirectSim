32a99f46025400de2d63979a954d5a3e496c7a90
Alex Abate <abate@email.arizona.edu>
2014-02-26 11:11:37 -0700

    Improved program baoprogs/getpzconvf.cc's readability and commenting, and added a doxygen document for it: docs/extra/getpzconvf.dox. Also improved the install doxygen documentation in docs/extra/install.dox

--- baoprogs/getpzconvf.cc
+++ baoprogs/getpzconvf.cc
@@ -1,9 +1,25 @@
+/**
+ * @file  getpzconvf.cc
+ * @brief Compute the photo-z convolution function given the photo-z - true-z 
+ *        distribution within a given redshift range
+ *
+ * @todo histogram bins should be option to program
+ * @todo not sure if zp-zs or (zp-zs)/(1+zs) should be added to histogram?
+ * 
+ * @author Alex Abate
+ * Contact: abate@email.arizona.edu
+ *
+ * Created on: Feb 2011
+ * @date 2011
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
 #include <fstream>
 
+// SOPHYA
 #include "sopnamsp.h"
 #include "histinit.h"
 #include "hisprof.h"
@@ -16,248 +32,249 @@
 #include "fiosinit.h"
 #include "tarray.h"
 
+
+// DirectSim
 #include "geneutils.h"
 #include "cat2grid.h"
 
-/*  
-
- Compute photo-z convolution function from the z-range
- given in a *_subinfo.txt file (produced by subfromfull)
- 
- A Abate - Feb 2011
- 
-*/
 
 void usage(void);
-void usage(void)
-	{
-	cout << endl<<" Usage: getpzconf [...options...]" << endl<<endl;
+void usage(void) {
+	
+	cout << endl<<" Usage: getpzconvf [...options...]              "<<endl<<endl;
 		
-	cout << "  Compute photo-z convolution function from the z-range"<<endl;
-	cout << "  given in a *_subinfo.txt file (produced by subfromfull)"<<endl;
-	cout << "  "<<endl;
+	cout << "  Compute photo-z convolution function from the redshift   "<<endl;
+	cout << "  range given in a *_subinfo.txt file (produced by the     "<<endl;
+	cout << "  program subfromfull)                                     "<<endl;
+	cout << endl;
 		
-	cout << " -O : ObsCat: FITS file containing observed catalog(s) (up to 2)"<<endl;
-	cout << " -Z : SubInfoFile: *_subinfo.txt file to read in"<<endl;
-	cout << " -o : PZConvFile: photo-z convolution function text filename"<<endl;
-	cout << " -z : ZOCol,ZSCol: read OBSERVED redshifts from column labeled ZOCol, SPECTRO redshifts from column labeled ZSCol"<<endl;
-	cout <<endl;
+	cout << " -O : obs_cat_file: FITS file containing observed catalog(s) "<<endl;
+	cout << "                    (up to 2 separated by commas)            "<<endl;
+	cout << " -Z : z_range_file: *_subinfo.txt file to read in            "<<endl;
+	cout << " -o : out_file: file to write photo-z convolution function to"<<endl;
+	cout << " -z : obs_z_col,true_z_col: read OBSERVED redshifts from     "<<endl;
+	cout << "      column labeled obs_z_col, true redshifts from column   "<<endl;
+	cout << "      labeled true_z_col                                     "<<endl;
+	cout << endl;
 	}
 
-void StringSplit(string str, string delim, vector<string>& results)
-	{
-	int cutAt;
-	while( (cutAt = str.find_first_of(delim)) != str.npos )
-		{
-		if(cutAt > 0)
-			results.push_back(str.substr(0,cutAt));
-		str = str.substr(cutAt+1);
-		}
-		if(str.length() > 0)
-			results.push_back(str);
-		
-	}
 
-int main(int narg, char *arg[])
-	{
+
+int main(int narg, char *arg[]) {
 
 	SophyaInit();
 	FitsIOServerInit();
   
-	cout << " ==== setting defaults ===="<<endl;
-	string obscats,subinfo,pzconv;
-	string ObCat1,ObCat2;
-	int NC=0;
-	string ZCol; 
-	bool Zsp=false;
-	string ZOCol = "zp"; // by default OBSERVED redshift column labelled "zp" is read in
-	string ZSCol = "zs"; // by default SPECTRO redshift column labelled "zs" is read in
+	string obscats;        // observed catalog(s) to read in (up to 2)
+	string z_range_file;   // *_subinfo.txt file containing redshift range
+	string outfile;        // output file
+	string obs_cat1;       // first catalog to read in
+	string obs_cat2;       // second catalog to read in (if specified)
+	int num_cats = 0;      // number of catalogs to read in
+	string zcols;          // string to get column names from 'z' argument
+	bool getColNames = false; // if true new redshift column names were specifed
+	string obs_z_col = "zp";  // by default OBSERVED z column called "zp" is read 
+	string true_z_col = "zs"; // by default SPECTRO z column called "zs" is read 
 
 	//--- decoding command line arguments 
-	cout << " ==== decoding command line arguments ===="<<endl;
 	char c;
-	while((c = getopt(narg,arg,"hO:Z:o:z:")) != -1) 
-	{
-	switch (c) 
-		{
-		case 'O' :
-			obscats = optarg;
-			break;
-		case 'Z' :
-			subinfo = optarg;
-			break;
-		case 'o' :
-			pzconv = optarg;
-			break;
-		case 'z' :
-			ZCol = optarg;
-			break;
-		case 'h' :
-		default :
-			usage(); return -1;
-		}
-	}
+	while((c = getopt(narg,arg,"hO:Z:o:z:")) != -1) {
+	    switch (c) {
+		    case 'O' :
+			    obscats = optarg;
+			    break;
+		    case 'Z' :
+			    z_range_file = optarg;
+			    break;
+		    case 'o' :
+			    outfile = optarg;
+			    break;
+		    case 'z' :
+			    zcols = optarg;
+			    getColNames = true;
+		 	    break;
+		    case 'h' :
+		    default :
+			    usage(); return -1;
+		    }
+	    }
 	
 
+    // get catalog(s) names
 	string delim=",";
-	vector<string> results;
-	StringSplit(obscats,delim,results);
-	vector<string>::iterator i;
-	i = results.begin();
-	ObCat1=*i;
-	i++;
-	NC++;
-	if (i!=results.end())
-		{
-		ObCat2=*i;
-		i++;
-		NC++;
-		}
+	vector<string> cats;
+	stringSplit(obscats, delim, cats);
+	obs_cat1 = cats[0];
+	if (cats.size()>1) {
+	    obs_cat2 = cats[1];
+	    num_cats++;
+	    }
+
 		
 	// get two z column names
-	if (Zsp)	
-		{ 
-		string delim=",";
+	if (getColNames) {
 		vector<string> results;
-		StringSplit(ZCol,delim,results);
-		vector<string>::iterator i;
-		i = results.begin();
-		ZOCol=*i;
-		i++;
-		if (i!=results.end())
-			ZSCol=*i;
+		stringSplit(zcols, delim, results);
+		obs_z_col = results[0];
+		if (results.size()>1)
+		    true_z_col = results[1];
 		}
 		
-	cout << " ==== finished decoding command line arguments ==== "<<endl<<endl;
 	
-	cout << "     Printing command line arguments ... "<<endl<<endl;
+	cout << "     Printing command line arguments ...             "<<endl<<endl;
 	
-	cout << "     Reading in "<<NC<<" observed catalogs, filename";
-	if (NC<2)
-		cout <<" is "<<ObCat1<<endl;
+	cout << "     Reading in "<< num_cats <<" observed catalog(s), from file";
+	if (num_cats<2)
+		cout << obs_cat1 <<endl;
 	else
-		cout <<"s are "<<ObCat1<<" and "<<ObCat2<<endl;
+		cout <<"s "<< obs_cat1 <<" and "<< obs_cat2 <<endl;
 
-	cout << "     Photometric redshifts to be read from "<<ZOCol<<" column, spectroscopic from "<<ZSCol<<" column"<<endl;
-	cout << "     Z range will be read from "<<subinfo<<endl;
-	cout << "     Photo-z convolution function will be saved to "<<pzconv<<endl;
-	cout <<endl;
+	cout << "     Photometric redshifts will be read from "<< obs_z_col;
+	cout << " column, true redshifts from "<< true_z_col <<" column     "<<endl;
+	cout << "     Redshift range will be read from "<< z_range_file      <<endl;
+	cout << "     Photo-z convolution function will be saved to "<< outfile <<endl;
+	cout << endl;
 	
-  try {
-		/* READ IN SUBINFO FILE*/
-		cout <<"0/ Read in subinfo file "<<subinfo<<endl;
-		ifstream ifs(subinfo.c_str());
+	
+    try {
+		
+		
+		// Read in redshift range from file
+		cout <<"     Read in redshift range from file "<< z_range_file <<endl;
+		ifstream ifs(z_range_file.c_str());
 		Array B;
 		sa_size_t nr, nc;
 		B.ReadASCII(ifs,nr,nc);
 		double minz = B(0,0);
 		double zc = B(1,0);
 		double maxz = B(2,0);
-		cout <<"    Sub-array bounds: "<<minz<<"< z <"<<maxz<<endl;
-		cout <<"    Sub-array center: zc = "<<zc<<endl;
+		cout <<"    Redshift bounds: "<< minz <<"< z <"<< maxz <<endl;
+		cout <<"    Redshift center: zc = "<< zc <<endl;
 
-		/* INITIALISE HISTOGRAM */
-		int nbin=200;
-		double mindz=-2, maxdz=2;
+
+		// Initialize histogram
+		// @todo histogram bins should be option to program
+		int nbin = 200;
+		double mindz = -2, maxdz = 2;
 		Histo deltaz(mindz, maxdz, nbin);
-		cout <<endl;
+		cout << endl;
   
-		/* READ IN CATALOG FITS FILE */
-		cout <<"1/ Read in observed catalog "<<ObCat1<<endl;
-		FitsInOutFile fin(ObCat1,FitsInOutFile::Fits_RO);
+  
+		// Read in observed catalog
+		cout <<"     Read in observed catalog "<< obs_cat1;
+		if (num_cats>1)
+		    cout << " 1 of 2" <<endl;
+		else
+		    cout << endl;
+		FitsInOutFile fin(obs_cat1, FitsInOutFile::Fits_RO);
 		fin.MoveAbsToHDU(2);
 		SwFitsDataTable dt(fin,512,false);
 		DataTableRow row = dt.EmptyRow();
 		sa_size_t ng = dt.NEntry();
-		sa_size_t Izs = dt.IndexNom(ZSCol);
-		sa_size_t Izp = dt.IndexNom(ZOCol);
+		sa_size_t Izs = dt.IndexNom(true_z_col);
+		sa_size_t Izp = dt.IndexNom(obs_z_col);
 		double minzo,maxzo,minzop,maxzop;
 		dt.GetMinMax(Izs,minzo,maxzo); 
 		dt.GetMinMax(Izp,minzop,maxzop); 
-		cout <<"    "<<ng<<" galaxies in the catalog"<<endl;
-		cout <<"    Min z of OBS catalog = "<<minzo<<", max z of OBS catalog = "<<maxzo<<endl;
-		cout <<"    Min zp of OBS catalog = "<<minzop<<", max zp of OBS catalog = "<<maxzop<<endl;
-		cout <<endl;
+		cout <<"     "<< ng <<" galaxies in the catalog"<<endl;
+		cout <<"     Min true z of OBS catalog = "<< minzo;
+		cout <<", max true z of OBS catalog = "<< maxzo <<endl;
+		cout <<"     Min photo-z of OBS catalog = "<< minzop;
+		cout <<", max photo-z of OBS catalog = "<< maxzop <<endl;
+		cout << endl;
 		
-		/* LOOP OVER CATALOG FITS FILE AND ADD TO HISTO */
-		cout <<"2/ Loop over catalog and add to Histo "<<endl;
-		for (sa_size_t i=0; i<ng; i++)
-			{
+		
+		// Loop over catalog and add redshift difference to histogram
+		cout <<"     Loop over catalog and add to Histo "<<endl;
+		for (sa_size_t i=0; i<ng; i++) {
+			
 			dt.GetRow(i,row);
-			double zs=row[Izs];
-			double zp=row[Izp];
-			double dz = zp - zs;
-			if (zs>=minz&&zs<maxz)
+			double zs = row[Izs];
+			double zp = row[Izp];
+			double dz = zp - zs; // should this be /(1+zs)??
+			
+			// if redshift is within redshift range that was read in
+			if ( zs>=minz && zs<maxz )
 				deltaz.Add(dz);
 			}
 			
-		if (NC>1)
-			{
-			/* READ IN 2nd CATALOG FITS FILE */
-			cout <<"1a/ Read in 2nd observed catalog "<<ObCat2<<endl;
-			FitsInOutFile fin2(ObCat2,FitsInOutFile::Fits_RO);
+			
+	    // Now check if there's a second catalog and repeat
+		if (num_cats>1) {
+			
+			// Read in 2nd catalog
+			cout <<"     Read in observed catalog "<< obs_cat2 <<" 2 of 2"<<endl;
+			FitsInOutFile fin2(obs_cat2, FitsInOutFile::Fits_RO);
 			fin2.MoveAbsToHDU(2);
 			SwFitsDataTable dt2(fin2,512,false);
 			DataTableRow row2 = dt2.EmptyRow();
 			sa_size_t ng2 = dt2.NEntry();
-			sa_size_t Izs2 = dt2.IndexNom("z");
-			sa_size_t Izp2 = dt2.IndexNom(ZCol);
+			sa_size_t Izs2 = dt2.IndexNom(true_z_col);
+		    sa_size_t Izp2 = dt2.IndexNom(obs_z_col);
 			double minzo2,maxzo2,minzop2,maxzop2;
 			dt2.GetMinMax(Izs2,minzo2,maxzo2); 
 			dt2.GetMinMax(Izp2,minzop2,maxzop2); 
-			cout <<"    "<<ng2<<" galaxies in the catalog"<<endl;
-			cout <<"    Min z of OBS catalog = "<<minzo2<<", max z of OBS catalog = "<<maxzo2<<endl;
-			cout <<"    Min zp of OBS catalog = "<<minzop2<<", max zp of OBS catalog = "<<maxzop2<<endl;
-			cout <<endl;
+			cout <<"     "<< ng2 <<" galaxies in catalog 2"<<endl;
+			cout <<"     Min  true z of OBS catalog = "<< minzo2;
+			cout <<", max true z of OBS catalog = "<< maxzo2 <<endl;
+			cout <<"     Min photo-z of OBS catalog = "<< minzop2;
+			cout <<", max photo-z of OBS catalog = "<< maxzop2 <<endl;
+			cout << endl;
 			
-			cout <<"1b/ Loop over 2nd catalog and add to Histo "<<endl;
-			/* LOOP OVER CATALOG FITS FILE AND ADD TO HISTO */
-			for (sa_size_t i=0; i<ng2; i++)
-				{
+			
+			// Loop over catalog and add redshift difference to histogram
+			cout <<"     Loop over catalog 2 and add to Histo "<<endl;
+			for (sa_size_t i=0; i<ng2; i++) {
+				
 				dt2.GetRow(i,row2);
 				double zs=row2[Izs2];
 				double zp=row2[Izp2];
-				double dz = zp - zs;
-				if (zs>=minz&&zs<maxz)
+				double dz = zp - zs;// should this be /(1+zs)??
+			
+			    // if redshift is within redshift range that was read in
+				if ( zs>=minz && zs<maxz )
 					deltaz.Add(dz);
 				}
 			
 			}
 		
-		/* WRITE HISTO TO TEXT FILE*/
 		
+		// Write histogram to a file
 		ifstream inp;
 		ofstream outp;
-		inp.open(pzconv.c_str(), ifstream::in);
+		inp.open(outfile.c_str(), ifstream::in);
 		inp.close();
-		if(inp.fail())
-			{
+		if (inp.fail()) {
+			
 			inp.clear(ios::failbit);
-			cout << "    Writing to file ..." << pzconv.c_str() << " and "<<pzconv<<endl;
-			outp.open(pzconv.c_str(), ofstream::out);
-			for(int_4 i=0;i<nbin;i++)
-				{
-			r_8 bc=deltaz.BinCenter(i);
-			r_8 nz=deltaz.operator()(i);
+			cout << "     Writing to file ..." << outfile.c_str() << endl;
+			outp.open(outfile.c_str(), ofstream::out);
+			
+			for (int_4 i=0; i<nbin; i++) {
+				
+			    r_8 bc = deltaz.BinCenter(i);
+			    r_8 nz = deltaz.operator()(i);
 		
-			outp <<bc<<"      "<<nz<<endl;
-			}
-		outp.close();
-		}
-	else
-		cout << "Error...file """ << pzconv.c_str() << """ exists" << endl;
+			    outp << bc <<"  "<< nz <<endl;
+			    }
+		    outp.close();
+		    }
+	    else
+		    cout << "Error...file """ << outfile.c_str() << """ exists" << endl;
 		
-	cout << endl;
-  }
-  catch(PThrowable exc ) {
-    cerr << "getpzconf.cc , Catched exception: \n" << exc.what() << endl;
-  }
-  catch(std::exception ex) {
-    cerr << "getpzconf.cc , Catched exception ! " << (string)(ex.what()) << endl;
-  }
-  catch(...) {
-    cerr << "getpzconf.cc , Catched ... ! " << endl;
-  }
+	    cout << endl;
+      }
+      
 
-cout << "--------------- getpzconf.cc / END --------------------- " << endl;
+catch(PThrowable exc ) {
+    cerr << "getpzconvf.cc , Catched exception: \n" << exc.what() << endl;
+    }
+catch(std::exception ex) {
+    cerr << "getpzconvf.cc , Catched exception ! " << (string)(ex.what()) << endl;
+    }
+catch(...) {
+    cerr << "getpzconvf.cc , Catched ... ! " << endl;
+    }
+
+cout << "--------------- getpzconvf.cc / END --------------------- " << endl;
 }
