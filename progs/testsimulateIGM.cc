8bab2f5fc7fe2cfeae78dae6f0d79e4160bd58b0
Lidens Cheng <lidenscheng@lsst00.physics.arizona.edu>
2015-01-08 20:09:26 -0700

    Updated Matt's wavelength intervals

--- progs/testsimulateIGM.cc
+++ progs/testsimulateIGM.cc
@@ -63,13 +63,21 @@ int main()
 
     int nmax = 10;                                  //Highest Lyman Line to go to
     int nLoS = 400;                                 //The number of lines of sight to examine for each source redshift
-    double zSource = 3.00;                          //Starting redshift of the background source
+    double zSource = 1.8;                           //Starting redshift of the background source
 
-    double lambdaMinA = 3000.;                      //Minimum WL in Angstroms (LSST minimum wavelength)
-    double lambdaMaxA = 100. + 1250.*(1+zSource);   //Maximum WL in Angstroms (About 100A past Lya from an absorber at zSource) (<11,000A)
+
+    double lambdaMinA = 3000.;                    //Minimum WL in Angstroms (LSST minimum wavelength)
+    double lambdaMaxA = 100. + 1250.*(1+zSource); //Maximum WL in Angstroms (About 100A past Lya from an absorber at zSource) (<11,000A)
+
+// Incorrect way for min and max wavelength 
+//    double lambdaMinA = 3000.*(1.+zSource-0.1)/(zSource+1.);         //Minimum WL in Angstroms (LSST minimum wavelength)
+//    double lambdaMaxA = 3000.;                      //Maximum WL in Angstroms (About 100A past Lya from an absorber at zSource) (<11,000A)
+
+
     double lambdaResA = 0.1;                        //WL Resolution in Angstroms
+//    double lambdaResA = 0.125;                        //WL Resolution in Angstroms
 
-    double zAbsorberMax = zSource + zSource*0.1;    //Maximum absorber redshift
+    double zAbsorberMax = zSource;// + zSource*0.1;    //Maximum absorber redshift
     double zAbsorberStart = 0.0;                    //Starting absorber redshift   
 
     string transFileName = "";                      //File name for the Transmission file
@@ -81,19 +89,50 @@ int main()
     transFileName = outputPath + "Transmission_" + ss.str();
     LoSFileName = outputPath + "LineOfSightData_" + ss.str();
 
+    string avgNAbsorbsOutFile = outputPath + "avgNAbsorbers.out";
 
     // ********************************
     // Initialize Clock
     // ********************************
 
+/*
     clock_t starttime, endtime;
     starttime = clock();
+*/
 
+    // ********************************
+    // Count number of Absorbers per redshift
+    // ********************************
+/*
+    ofstream countAbsorbs;
+    countAbsorbs.open(avgNAbsorbsOutFile.c_str());
+
+    int totalAbs = 0;
+
+    for(int j=0; j<16; j++) {
+        for(int i=0; i<nLoS; i++) {
+            pdist.simulateLineOfSight(zAbsorberStart, zAbsorberMax+j*0.1, redshifts, dopplerPars,
+                                      columnDensities, outfile);
+
+            totalAbs += columnDensities.size();
+
+            redshifts.clear();
+            dopplerPars.clear();
+            columnDensities.clear();
+        }
+
+        countAbsorbs << zAbsorberMax+j*0.1 << "     " << totalAbs/400. << endl;
+        totalAbs = 0;
+    }
+
+    countAbsorbs.close();
+// */
 
     // ********************************
     // Simulate nLoS lines of sight and record them to a file
     // ********************************
 
+// /*
     ofstream LoSData;
     LoSData.open(LoSFileName.c_str());
 
@@ -127,11 +166,13 @@ int main()
 
     LoSData.close();
 
+// */
 
     // ********************************
     // Make and write the wavelength vector
     // ********************************
 
+// /*
     ofstream transmissionData;
     transmissionData.open(transFileName.c_str());
 
@@ -146,16 +187,23 @@ int main()
         wavelengths.push_back(wavelengthTemp);
 
         transmissionData << wavelengthTemp << " ";
+//        transmissionData << wavelengthTemp << endl;
 
         lambdaCA += lambdaResA;
     }
 
     transmissionData << endl;
+//    transmissionData << " ";
 
+// */
 
     // ********************************
     // Calculate the Transmission
     // ********************************
+
+// /*
+
+    int lineLength = 0;
 
     for(int k=0; k<nLoS; k++) {
         vector<double> transmission;
@@ -166,16 +214,24 @@ int main()
 
         for(int i=0; i<transmission.size(); i++) {
             transmissionData << transmission[i] << " ";
+//            transmissionData << transmission[i] << endl;
+
+
         }
         transmissionData << endl;
+//	transmissionData << " ";
+
+        cout << transmission.size() << endl;
     }
 
     transmissionData.close();
+// */
+
 
     // ********************************
     // End clock and output time
     // ********************************
-
+/*
     endtime = clock();
     float runTimeTicks = ((float)endtime - (float)starttime);
     float runTimeSeconds = runTimeTicks / CLOCKS_PER_SEC;
@@ -188,7 +244,7 @@ int main()
     clockOutput.close();
     
 
+*/
     return 0;
 };
-
 
